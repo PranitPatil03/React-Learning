400a93d9af83c43a21c43d9e119ef8a09c3b4332
pranit <patilpranit3112@gmail.com>
2022-07-16 18:10:21 +0530

    Advanced Dark Mode updated

--- Lessons/first-react-app/src/App.js
+++ Lessons/first-react-app/src/App.js
@@ -1,17 +1,32 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
-import About from './components/About';
+import TextForm from './components/TextForm';
+// import About from './components/About';
+import React, { useState } from 'react'
 
 function App() {
+
+  const [mode ,setMode]= useState("light");
+
+  const togglemode= ()=>{
+    if(mode==="light"){
+      setMode("dark"); 
+      document.body.style.backgroundColor='black'
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor='white'
+    }
+  }
+
   return (
     <>
       {/* <Navbar /> */}
-      <Navbar title="text-app" />
-      <div className="container">
-      {/* <TextForm heading="TextUtiles"/> */}
-      <About />
+      <Navbar title="text-app" mode={mode} togglemode={togglemode}/>
+      <div className="container my-3">
+      <TextForm heading="TextUtiles" mode={mode}/>
+      {/* <About /> */}
       </div>
     </>
   );
--- Lessons/first-react-app/src/components/TextForm.js
+++ Lessons/first-react-app/src/components/TextForm.js
@@ -36,18 +36,21 @@ export default function TextForm(props) {
 
     return (
         <>
-        <div>
+        <div className="container" style={{color:props.mode==='dark'?'white':'black'}} >
             <h2>{props.heading}</h2>
             <div className="mb-3">
-                <textarea className="form-control" value={Text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="10"></textarea>
+                <textarea style={{backgroundColor:props.mode === 'dark'?'white':'dark',color:props.mode==='dark'?'black':'grey'}} className="form-control" value={Text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="10"></textarea>
             </div>
-            <button className="btn btn-primary mx-3" onClick={OnClickUppercase}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-3" onClick={OnClickLowercase}>Convert to Lowercase</button>
-            <button className="btn btn-primary mx-3" onClick={OnClickClear}>Clear All</button>
-            <button className="btn btn-primary mx-3" onClick={OnClickUndo}>Undo</button>
-            <button className="btn btn-primary mx-3" onClick={OnClickRedo}>Redo</button>
+            <div className="btns">
+            <button className={`btn btn-${props.mode === 'light'?'dark':'light'} mx-3`} onClick={OnClickUppercase}>Convert to Uppercase</button>
+            <button className={`btn btn-${props.mode === 'light'?'dark':'light'} mx-3`} onClick={OnClickLowercase}>Convert to Lowercase</button>
+            <button className={`btn btn-${props.mode === 'light'?'dark':'light'} mx-3`} onClick={OnClickClear}>Clear All</button>
+            <button className={`btn btn-${props.mode === 'light'?'dark':'light'} mx-3`} onClick={OnClickUndo}>Undo</button>
+            <button className={`btn btn-${props.mode === 'light'?'dark':'light'} mx-3`} onClick={OnClickRedo}>Redo</button>
+            </div>
+            
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
             <h2>Text Summary</h2>
             <p> {Text.split(" ").length} Words And {Text.length} Characters</p>
             <p>{0.008 * Text.split(" ").length} Minutes to Read.</p>
