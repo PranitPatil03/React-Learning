3fe9476db0e4bc91d0a2fda3e35ab524b1383b4d
pranit <patilpranit3112@gmail.com>
2022-07-19 01:04:37 +0530

    Router

--- Lessons/first-react-app/src/App.js
+++ Lessons/first-react-app/src/App.js
@@ -2,87 +2,94 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
 import React, { useState } from 'react'
 import Alert from './components/Alert';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
 
-  const [mode ,setMode]= useState("light")
+  const [mode, setMode] = useState("light")
   const [alert, setAlert] = useState(null)
 
-  const showAlert = (msg,type)=>{
+  const showAlert = (msg, type) => {
     setAlert({
-      msg:msg,
-      type:type
+      msg: msg,
+      type: type
     })
     setInterval(() => {
       setAlert(null)
     }, 2000);
   }
 
-  const togglemode= ()=>{
-    if(mode==="light"){
-      setMode("dark"); 
-      document.body.style.backgroundColor='black'
-      showAlert("Dark mode is enabled" ,"success")
+  const togglemode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = 'black'
+      showAlert("Dark mode is enabled", "success")
     }
-    else{
+    else {
       setMode("light");
-      document.body.style.backgroundColor='white'
-      showAlert("Light mode is enabled" ,"success")
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light mode is enabled", "success")
     }
   }
 
-  const togglToBlue= ()=>{
-    if(mode==="light"){
-      setMode("#0d6efd"); 
-      document.body.style.backgroundColor='#0d6efd'
-      document.body.style.color='black'
-      showAlert("Dark mode is enabled" ,"success")
+  const togglToBlue = () => {
+    if (mode === "light") {
+      setMode("#0d6efd");
+      document.body.style.backgroundColor = '#0d6efd'
+      document.body.style.color = 'black'
+      showAlert("Dark mode is enabled", "success")
     }
-    else{
+    else {
       setMode("light");
-      document.body.style.backgroundColor='white'
-      showAlert("Light mode is enabled" ,"success")
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light mode is enabled", "success")
     }
   }
-  const togglToGreen= ()=>{
-    if(mode==="light"){
-      setMode("#198754"); 
-      document.body.style.backgroundColor='#198754'
-      document.body.style.color='black'
-      showAlert("Dark mode is enabled" ,"success")
+  const togglToGreen = () => {
+    if (mode === "light") {
+      setMode("#198754");
+      document.body.style.backgroundColor = '#198754'
+      document.body.style.color = 'black'
+      showAlert("Dark mode is enabled", "success")
     }
-    else{
+    else {
       setMode("light");
-      document.body.style.backgroundColor='white'
-      showAlert("Light mode is enabled" ,"success")
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light mode is enabled", "success")
     }
   }
-  const togglToYellow= ()=>{
-    if(mode==="light"){
-      setMode("#ffc107"); 
-      document.body.style.backgroundColor='#ffc107'
-      document.body.style.color='black'
-      showAlert("Dark mode is enabled" ,"success")
+  const togglToYellow = () => {
+    if (mode === "light") {
+      setMode("#ffc107");
+      document.body.style.backgroundColor = '#ffc107'
+      document.body.style.color = 'black'
+      showAlert("Dark mode is enabled", "success")
     }
-    else{
+    else {
       setMode("light");
-      document.body.style.backgroundColor='white'
-      showAlert("Light mode is enabled" ,"success")
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light mode is enabled", "success")
     }
   }
-  
+
   return (
     <>
-      {/* <Navbar /> */}
+      <BrowserRouter>
       <Navbar title="text-app" mode={mode} togglemode={togglemode} togglToBlue={togglToBlue} togglToGreen={togglToGreen} togglToYellow={togglToYellow} />
-      <Alert alert={alert}/>
-      <div className="container my-3">
-      <TextForm heading="TextUtiles" mode={mode} showAlert={showAlert}/>
-      {/* <About /> */}
-      </div>
+      <Alert alert={alert} />
+      <Routes>
+        <Route path="/" element={<TextForm heading="TextUtiles" mode={mode} showAlert={showAlert} />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
--- Lessons/first-react-app/src/components/Navbar.js
+++ Lessons/first-react-app/src/components/Navbar.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
 
 export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">{props.title}</a>
+        <Link className="navbar-brand" to="/">{props.title}</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
+              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">About</a>
+              <Link className="nav-link" to="/about">About</Link>
             </li>
           </ul>
           <form className="d-flex" role="search">
@@ -23,16 +24,16 @@ export default function Navbar(props) {
             <button className="btn btn-outline-success" type="submit">Search</button>
           </form>
           <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
-            <button onClick={props.togglemode} type="button" class={`btn btn-${props.mode === 'light' ? 'dark' : 'light'}`}>{props.mode === 'light' ? 'dark' : 'light'}</button>
+            <button onClick={props.togglemode} type="button" className={`btn btn-${props.mode === 'light' ? 'dark' : 'light'}`}>{props.mode === 'light' ? 'dark' : 'light'}</button>
           </div>
           <div className={`form-check form-switch text-${{color:props.mode === '#0d6efd' ? 'light' : 'dark'}}`}>
-            <button onClick={props.togglToBlue} type="button" class={`btn btn-${props.mode === 'light' ? 'primary' : 'light'}`}>{props.mode === 'light' ? 'blue' : 'light'}</button>
+            <button onClick={props.togglToBlue} type="button" className={`btn btn-${props.mode === 'light' ? 'primary' : 'light'}`}>{props.mode === 'light' ? 'blue' : 'light'}</button>
           </div>
           <div className={`form-check form-switch text-${{color:props.mode === '#198754' ? 'light' : 'dark'}}`}>
-            <button onClick={props.togglToGreen} type="button" class={`btn btn-${props.mode === 'light' ? 'success' : 'light'}`}>{props.mode === 'light' ? 'green' : 'light'}</button>
+            <button onClick={props.togglToGreen} type="button" className={`btn btn-${props.mode === 'light' ? 'success' : 'light'}`}>{props.mode === 'light' ? 'green' : 'light'}</button>
           </div>
           <div className={`form-check form-switch text-${{color:props.mode === '#ffc107' ? 'light' : 'dark'}}`}>
-            <button onClick={props.togglToYellow} type="button" class={`btn btn-${props.mode === 'light' ? 'warning' : 'light'}`}>{props.mode === 'light' ? 'yellow' : 'light'}</button>
+            <button onClick={props.togglToYellow} type="button" className={`btn btn-${props.mode === 'light' ? 'warning' : 'light'}`}>{props.mode === 'light' ? 'yellow' : 'light'}</button>
           </div>
         </div>
       </div>
