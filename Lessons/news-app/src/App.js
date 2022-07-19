6dc71feddd85628fb24c1b7866ea30bffa9d087a
pranit <patilpranit3112@gmail.com>
2022-07-19 19:15:40 +0530

    news-app using class based Component

---
+++ Lessons/news-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        Hellow this is my first react class based components Project
+      </div>
+    )
+  }
+}
