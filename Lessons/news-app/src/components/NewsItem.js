0b12a195298e20e9212a4a0061520085e5d867b0
pranit <patilpranit3112@gmail.com>
2022-07-21 11:44:42 +0530

    fetch news & displayed

--- Lessons/news-app/src/components/NewsItem.js
+++ Lessons/news-app/src/components/NewsItem.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
+    constructor(){
+        super();
+        console.log()
+    }
+
     render() {
 
-        let {title,description,imgUrl} = this.props
+        let {title,description,imgUrl,newsUrl} = this.props
 
         return (
             <div className="container">
                 <div className="card" style={{width: "18rem"}}>
                     <img src={imgUrl} className="card-img-top" alt="..."/>
                         <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{description}</p>
-                            <a href="/" className="btn btn-sm btn-primary">Read more</a>
+                            <h5 className="card-title">{title}...</h5>
+                            <p className="card-text">{description}...</p>
+                            <a href={newsUrl} rel="opener" className="btn btn-sm btn-primary">Read more</a>
                         </div>
                 </div>
             </div>
